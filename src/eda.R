bd765b9c5ec7969c35a212d1fd043d755f14d379
Bruce Durling <bruce@mastodonc.com>
2020-02-14 14:10:03 +0000

    TODO: Find a good way to save the sankey

--- src/eda.R
+++ src/eda.R
@@ -634,6 +634,7 @@ print(sankeyNetwork(Links = links, Nodes = nodes, Source = "source",
                                      "#E15759","#FF9D9A","#79706E","#BAB0AC","#D37295","#FABFD2","#B07AA1","#D4A6C8","#9D7660","#D7B5A6", "#FFFFFF"])')
                     ))
 
+## TODO: Find a good way to save the sankey
 
 ## Individual historic sequences
 
