47ce2bbd205c528b4a045d46c67fc5e9ba812e8c
Peter <peter.andrewsbriscoe@gmail.com>
2021-01-14 15:30:43 +0000

    added in graph with all sample labels, upper limit 750

--- Joiners_age.R
+++ Joiners_age.R
@@ -13,12 +13,10 @@ mean(Period.Duration)
 periods$Period.End <- ymd(periods$Period.End)
 j=-5
 i=0
-median_bound=0
-mean_bound=0
-lower_bound
+
 
 pdf(output_file_test)
-facet_grid(rows=vars(i),cols=vars(j)) +
+
   for (i in 0:17){
   for (j in -5:5){
     print(
@@ -72,6 +70,33 @@ for (i in 0:17){
       labs(title = 'CDF for joiners') +
       coord_cartesian(xlim=c(0,(18-i)*365),ylim=c(0,1)) +
       theme_mastodon)
+  i=i+1
+}
+dev.off()
+
+pdf(output_file_test)
+for (i in 0:17){
+  print(
+    periods %>%
+      filter(Admission.Age == i) %>%
+      mutate(sample_label = year(Period.End - months(7) - days(12)) - 2019) %>%
+      filter(sample_label %in% -5:4) %>%
+      # mutate(sample_label = if_else(sample_label < 0, "Historic", "Projected")) %>%
+      mutate(sample_label = paste(sample_label)) %>%
+      # mutate(Period.Duration = Period.Duration + rnorm(1, sd = 7)) %>%
+      group_by(sample_label) %>%
+      mutate(cdf = ecdf(Period.Duration)(Period.Duration)) %>%
+      ungroup %>%
+      ggplot(aes(Period.Duration, cdf, group = sample_label, colour = sample_label)) +
+      theme(panel.grid.major = element_line(size = 0.1, linetype = "solid",colour = "darkgrey"))+
+      theme(panel.grid.minor = element_line(size = 0.1, linetype = "solid",colour = "darkgrey"))+
+      geom_vline(aes(xintercept=mean(Period.Duration),colour ="mean")) +
+      geom_vline(aes(xintercept=median(Period.Duration),colour ="median")) +
+      geom_line() +
+      scale_color_manual(values = tableau_color_pal("Tableau 20")(20)) +
+      labs(title = 'CDF for joiners') +
+      coord_cartesian(xlim=c(0,750),ylim=c(0,1)) +
+      theme_mastodon )
   i=i+1
 }
 dev.off()
