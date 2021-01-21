89c4ee50c245791f0c694835a57a7d9ec7903d2d
Peter <peter.andrewsbriscoe@gmail.com>
2021-01-21 14:46:13 +0000

    properly updated colours

--- Barcharts_for_ID_Pathways.R
+++ Barcharts_for_ID_Pathways.R
@@ -2,17 +2,29 @@ install.packages("tidyverse")
 
 library(tidyverse)
 
-i="2526-1"
-df <- periods%>%
+i="3549-1"
+df <- projected_episodes%>%
   filter(ID==i)
 
 pdf(output_file_test)
 ggplot(df, aes(reorder(Simulation,Period.Duration), Period.Duration))+
-          geom_bar(stat='identity',position = position_dodge(width = 1),width=0.1)+
+          geom_bar(stat='identity',aes(fill=Placement))+
           theme(axis.text.x=element_blank(),
-                text=element_text(size = 15)) +
-          facet_wrap(~Placement.Pathway,shrink=FALSE,scales="free_x")+
+                text=element_text(size = 15),
+                strip.text.x = element_text(size = 8, angle = 90)) +
+          scale_fill_manual(values=my.colours)+
+          facet_grid(~Placement.Pathway,scales='free_x',space='free_x')+
           labs(x= "Simulations",y='Period Duration', title = i)
 
  dev.off()
 
+ library(ggthemes)
+ all.placements <- c("A3", "A4", "A5", "A6", "H5", "K1", "K2", "M2", "M3", "P1", "P2",
+                     "Q1","Q2", "R1", "R2", "R3", "R5", "S1", "T0", "T4", "Z1", "OUT")
+ # Define the number of colors you want
+ nb.cols <- 21
+ my.colours <- colorRampPalette(tableau_color_pal("Tableau 20")(11))(nb.cols)
+ my.colours <- c(my.colours, "#888888")
+ names(my.colours) <- all.placements
+ 
+ 
