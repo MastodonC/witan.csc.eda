f2c7952a793f0b1087085c1bee538033b1c2ec05
Peter <peter.andrewsbriscoe@gmail.com>
2021-01-21 10:06:58 +0000

    fixed bar chart x axis to be the right size

--- Barcharts_for_ID_Pathways.R
+++ Barcharts_for_ID_Pathways.R
@@ -2,14 +2,17 @@ install.packages("tidyverse")
 
 library(tidyverse)
 
-i="3549-1"
+i="2526-1"
 df <- periods%>%
-  filter(ID==i)%>%
-  group_by(Placement.Pathway)
+  filter(ID==i)
+
 pdf(output_file_test)
 ggplot(df, aes(reorder(Simulation,Period.Duration), Period.Duration))+
-          geom_bar(stat='identity',width = 0.05)+
-          facet_wrap(~Placement.Pathway,scale = "free_x")+
-          labs(x= "Simulations",title = i)
+          geom_bar(stat='identity',position = position_dodge(width = 1),width=0.1)+
+          theme(axis.text.x=element_blank(),
+                text=element_text(size = 15)) +
+          facet_wrap(~Placement.Pathway,shrink=FALSE,scales="free_x")+
+          labs(x= "Simulations",y='Period Duration', title = i)
 
-dev.off()
+ dev.off()
+
