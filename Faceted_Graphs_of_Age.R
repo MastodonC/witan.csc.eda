66f46800b0f9f813dce8ef3af77e72ede1d8e35e
Peter <peter.andrewsbriscoe@gmail.com>
2021-02-01 14:56:48 +0000

    added file to make graphs faceted by age

---
+++ Faceted_Graphs_of_Age.R
@@ -0,0 +1,28 @@
+projected_episodes_file <- 'P:\\scc-episodes-2019-08-13-rewind-1yr-train-3yr-project-5yr-runs-100-seed-42-20201203-no-reject-sampling.csv'
+projected_episodes <- read.csv(projected_episodes_file)
+
+periods$Period.End <- ymd(periods$Period.End)
+
+chart_data <- periods %>%
+  mutate(sample_label = year(Period.End - months(7) - days(12)) - 2019) %>%
+  filter(sample_label %in% -5:4) %>%
+  # mutate(sample_label = if_else(sample_label < 0, "Historic", "Projected")) %>%
+  mutate(sample_label = factor(paste(sample_label), levels = as.character(-5:4))) %>%
+  # mutate(Period.Duration = Period.Duration + rnorm(1, sd = 7)) %>%
+  group_by(sample_label, Admission.Age) %>%
+  mutate(cdf = ecdf(Period.Duration)(Period.Duration)) %>%
+  ungroup
+
+ggplot(chart_data, aes(Period.Duration, cdf, group = sample_label, colour = sample_label)) +
+  facet_grid(sample_label~Admission.Age) +
+  theme(panel.grid.major = element_line(size = 0.1, linetype = "solid",colour = "darkgrey"))+
+  theme(panel.grid.minor = element_line(size = 0.1, linetype = "solid",colour = "darkgrey"))+
+  geom_vline(data = chart_data %>% group_by(sample_label, Admission.Age) %>% summarise(intercept = mean(Period.Duration)),
+             aes(xintercept=intercept,colour ="mean")) +
+  geom_vline(data = chart_data %>% group_by(sample_label, Admission.Age) %>% summarise(intercept = median(Period.Duration)),
+             aes(xintercept=intercept,colour ="median")) +
+  geom_line() +
+  scale_color_manual(values = tableau_color_pal("Tableau 20")(20)) +
+  labs(title = "CDF for joiners") +
+  coord_cartesian(xlim=c(0,750),ylim=c(0,1)) +
+  theme_mastodon
