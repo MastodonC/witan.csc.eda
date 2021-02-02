d4852bd41426f642f57c4f9f143dceac9d0f76fb
Seb Bailey <sebastian@mastodonc.com>
2021-02-02 17:24:54 +0000

    make suitable for calling faceted graph

--- src/update-episodes.R
+++ src/update-episodes.R
@@ -1,10 +1,12 @@
 library(dplyr)
 library(lubridate)
+library(ggplot2)
+library(ggthemes)
 
 data <- read.csv("~/Downloads/episodes-rewind-1yr-train-5yr-project-2yr-runs-100-seed-42.csv", 
                   header = TRUE)
 
-data %>%
+periods <- data %>%
   mutate(Start = ymd(Start), End = ymd(End)) %>%
   group_by(ID) %>%
   mutate(Period.Start = min(Start), Period.End = max(End)) %>%
